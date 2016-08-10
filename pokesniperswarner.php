304c78b794818b136d6c866990247922bd3ab9db
Antoine Mineau <am2@ohwee.fr>
2016-08-10 14:22:02 +0200

    fix api parsing

--- pokesniperswarner.php
+++ pokesniperswarner.php
@@ -4,7 +4,7 @@ const IV = 90;
 
 function main()
 {
-	while(1) {
+	while(true) {
 		warner();
 		sleep(20);
    	}
@@ -16,11 +16,12 @@ function warner()
 	$results = file_get_contents('http://pokesnipers.com/api/v1/pokemon.json');
 	$json = json_decode($results, true);
 
-	foreach ($json as $row) {
+    foreach ($json['results'] as $row) {
 		if ($row['iv'] >= IV) {
 			var_dump($row);
-        }
+		}
     }
 }
 
 main();
+?>
